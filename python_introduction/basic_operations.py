daa47d5ef3accef12b195a94bfaa0ff3e5bf4193
Timilehin Ojomo <timilehinojomo@gmail.com>
2024-12-21 08:56:08 +0100

    added README.md

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = int(input("Enter the first number"))
+number2 = int(input("Enter the second number"))
+
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+print("sum of", number1, "and", number2, "is", sum)
+print("difference of", number1, "and", number2, "is", difference)
+print("product of", number1, "and", number2, "is", product)
