881b24e4ece1b8eb91cb02edc57fd1c636236e3f
Timilehin Ojomo <timilehinojomo@gmail.com>
2025-01-11 17:30:19 +0100

    modified arithmetic_operation.py so perform_operation() checks for dividing by zero

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,11 +1,15 @@
 
-def perform_operation(num1, num2, operation):
-    if operation == "add":
-        result = num1 + num2
-    elif operation == "subtract":
-        result = num1 - num2
-    elif operation == "multiply":
-        result = num1 * num2
-    elif operation == "divide":
-        result = num1 / num2
+def perform_operation(num1, num2, operation):  
+    match operation:
+        case "add":
+            result = float(num1) + float(num2)
+        case "subtract":
+            result = float(num1) - float(num2)
+        case "multiply":
+            result = float(num1) * float(num2)
+        case "divide":
+            if num2 == 0:
+                print("can't divide by zero")
+            elif num2 != 0:
+                result = float(num1) / float(num2)
     return result
