7b343d7cb814f434b0173c97ca80c49f6bd0baa9
Timilehin Ojomo <timilehinojomo@gmail.com>
2025-01-11 16:36:43 +0100

    added fns_and_dsa

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,11 @@
+
+def perform_operation(num1, num2, operation):
+    if operation == "add":
+        result = num1 + num2
+    elif operation == "subtract":
+        result = num1 - num2
+    elif operation == "multiply":
+        result = num1 * num2
+    elif operation == "divide":
+        result = num1 / num2
+    return result
