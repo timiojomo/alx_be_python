e1b798f20588c1ceed8315f6f7db416966041fe1
Timilehin Ojomo <timilehinojomo@gmail.com>
2025-01-11 17:56:36 +0100

    modified shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,7 +13,7 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            item = input("Enter the item to add to the list")
+            item = input("Enter the item to add:")
             shopping_list.append(item)
             pass
         elif choice == '2':
