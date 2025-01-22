8b483fc7c26b7233ae35624108a3be109810fdd9
Timilehin Ojomo <timilehinojomo@gmail.com>
2025-01-22 22:27:08 +0100

    created programming paradigm

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,19 @@
+
+class BankAccount:
+    def __init__(self, account_balance = 0):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        return self.account_balance + amount
+    
+    def withdraw(self, amount):
+        if self.account_balance >= amount:
+            return self.account_balance - amount
+        elif self.account_balance < amount:
+            return self.account_balance
+    
+    def display_balance(self):
+        print(self.account_balance)
+
+    
+        
