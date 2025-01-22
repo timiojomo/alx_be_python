628dd10c61b7107482c6fa5db763169d3140d150
Timilehin Ojomo <timilehinojomo@gmail.com>
2025-01-22 22:38:18 +0100

    modified bank_accounts.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,7 +13,7 @@ class BankAccount:
             return self.account_balance
     
     def display_balance(self):
-        print(f"Current Balance: {self.account_balance}")
+        print(f"Current Balance: ${float(self.account_balance)}")
 
     
         
