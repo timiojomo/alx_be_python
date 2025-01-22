5adc935e287072177c3d5c4a212443fd7eeed4b7
Timilehin Ojomo <timilehinojomo@gmail.com>
2025-01-22 23:03:39 +0100

    modified bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,7 +13,7 @@ class BankAccount:
             return False
     
     def display_balance(self):
-        print(f"Current Balance: ${float(self.account_balance)}")
+        print(f"Current Balance: ${float(self.account_balance):.2f}")
 
     
         
