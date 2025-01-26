14478b2f995673f43ac6e476f5615d7adfb522d8
Timilehin Ojomo <timilehinojomo@gmail.com>
2025-01-26 23:53:04 +0100

    added classic_static

--- oop/main.py
+++ oop/main.py
@@ -1,17 +1,17 @@
-from polymorphism_demo import Shape, Rectangle, Circle
-import math
+# from polymorphism_demo import Shape, Rectangle, Circle
+# import math
 
-def main():
-    shapes = [
-        Rectangle(10, 5),
-        Circle(7)
-    ]
+# def main():
+#     shapes = [
+#         Rectangle(10, 5),
+#         Circle(7)
+#     ]
 
-    for shape in shapes:
-        print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
+#     for shape in shapes:
+#         print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
     
 # from library_system import Book, EBook, PrintBook, Library
 
@@ -34,3 +34,17 @@ if __name__ == "__main__":
 
 # if __name__ == "__main__":
 #     main()
+
+from class_static_methods_demo import Calculator
+
+def main():
+    # Using the static method
+    sum_result = Calculator.add(10, 5)
+    print(f"The sum is: {sum_result}")
+
+    # Using the class method
+    product_result = Calculator.multiply(10, 5)
+    print(f"The product is: {product_result}")
+
+if __name__ == "__main__":
+    main()
