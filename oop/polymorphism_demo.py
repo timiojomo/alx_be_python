3544f3a2e53f8239091e0e71f6d7d8687d7e8ff9
Timilehin Ojomo <timilehinojomo@gmail.com>
2025-01-26 23:58:30 +0100

    a

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -8,9 +8,19 @@ class Shape:
         raise NotImplementedError
     
 class Rectangle(Shape):
+    def __init__(self):
+        super().__init__()
+
     def area(self, length, width):
-        return length * width
+        return self.length * self.width
+    
+    def __str__(self):
+        return super().__str__()
     
 class Circle(Shape):
+
+    def __init__(self):
+        super().__init__()
+
     def area(self, radius):
-        return math.pi * radius ** 2
+        return math.pi * self.radius ** 2
