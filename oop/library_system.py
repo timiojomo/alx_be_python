a050df4dcb1c7adff19a6122da4d676833697607
Timilehin Ojomo <timilehinojomo@gmail.com>
2025-01-26 23:59:59 +0100

    b

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,15 +3,24 @@ class Book:
         self.title = title
         self.author = author
 
+    def __str__(self):
+        pass
+
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
 
+    def __str__(self):
+        return super().__str__()
+
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
+
+    def __str__(self):
+        return super().__str__()
 
 class Library:
     
